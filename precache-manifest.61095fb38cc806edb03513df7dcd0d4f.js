b7d0a0136e758a62551eae3e7639d52137c73cf0
pavi199226 <pavi199226@gmail.com>
2020-06-29 11:29:59 +0200

    Updates

---
+++ precache-manifest.61095fb38cc806edb03513df7dcd0d4f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143d861a24a5f558b7484a2977af737b",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "264adfbfc4d54b33f913",
+    "url": "/mysite/static/css/main.e7fc19ee.chunk.css"
+  },
+  {
+    "revision": "c8b349a45e45aaff5ba9",
+    "url": "/mysite/static/js/2.44349147.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mysite/static/js/2.44349147.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "264adfbfc4d54b33f913",
+    "url": "/mysite/static/js/main.5bf0330e.chunk.js"
+  },
+  {
+    "revision": "1d99fe0da908ca569518",
+    "url": "/mysite/static/js/runtime-main.1d89857a.js"
+  },
+  {
+    "revision": "6a5e617a4b0e84cc689f7df2153e80ec",
+    "url": "/mysite/static/media/avataars.6a5e617a.svg"
+  },
+  {
+    "revision": "56ec4ee72a91e8f3d954db6cccbf8ab2",
+    "url": "/mysite/static/media/cabin.56ec4ee7.png"
+  },
+  {
+    "revision": "a29c0b43958bf3638364e15ff646fffa",
+    "url": "/mysite/static/media/cake.a29c0b43.png"
+  },
+  {
+    "revision": "e27c88e555910ddce50b6f1e275023a4",
+    "url": "/mysite/static/media/circus.e27c88e5.png"
+  },
+  {
+    "revision": "107d31e81b23d07ad38c7cc4d1f7ea27",
+    "url": "/mysite/static/media/game.107d31e8.png"
+  },
+  {
+    "revision": "dfa7703b841989c158a12839d3f39e85",
+    "url": "/mysite/static/media/img2.dfa7703b.jpg"
+  },
+  {
+    "revision": "7dfdcf559d331a0cdc05cc1d2b8c8233",
+    "url": "/mysite/static/media/safe.7dfdcf55.png"
+  },
+  {
+    "revision": "8fad4ff92040b17345896e0e777a79ad",
+    "url": "/mysite/static/media/submarine.8fad4ff9.png"
+  }
+]);
